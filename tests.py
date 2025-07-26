c4d5c7a444c2897f4bebdb07dd131474841f81b5
The-Truest-Repairman <brandonpekarek91@gmail.com>
2025-07-25 18:39:13 -0700

    Implement get_file_content function

--- tests.py
+++ tests.py
@@ -1,11 +1,14 @@
 #Import get_files_info from the module functions in the same directory
-from functions.get_files_info import get_files_info
-print("Test 1:")
-print(get_files_info("calculator", "."))
+from functions.get_file_content import get_file_content
+#print("Test: 0")
+#print(get_file_content("calculator", "lorem.txt"))
+
+print("\nTest 1:")
+print(get_file_content("calculator", "main.py"))
 
 print("\nTest 2:")
-print(get_files_info("calculator", "pkg"))
+print(get_file_content("calculator", "pkg/calculator.py"))
 print("\nTest 3:")
-print(get_files_info("calculator", "/bin"))
+print(get_file_content("calculator", "/bin/cat"))
 print("\nTest 4:")
-print(get_files_info("calculator", "../"))
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
